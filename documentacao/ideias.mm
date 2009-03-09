6f44e04b154afe521fafca69fc13a1170d8391ed
unknown <Tulio Ornelas@.(none)>
2009-03-09 18:48:32 -0300

    primeiro commit, adicionando mapa com levantamento de ideias

---
+++ documentacao/ideias.mm
@@ -0,0 +1,144 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1236005851546" ID="Freemind_Link_73256618" MODIFIED="1236006701843" TEXT="projeto final">
+<node CREATED="1236005864812" ID="_" MODIFIED="1236013936750" POSITION="right" TEXT="viagens">
+<cloud/>
+<node CREATED="1236593899187" ID="Freemind_Link_445291631" MODIFIED="1236595276921" TEXT="aplicativos similares">
+<node CREATED="1236594057640" ID="Freemind_Link_1748655468" MODIFIED="1236594058750" TEXT="http://naviagem.ig.com.br/site/index.asp">
+<node CREATED="1236594064234" ID="Freemind_Link_45047355" MODIFIED="1236594067750" TEXT="pontos fortes">
+<node CREATED="1236594102500" ID="Freemind_Link_674853283" MODIFIED="1236594274546" TEXT="comunidades (n&#xe3;o &#xe9; usado direito)"/>
+</node>
+<node CREATED="1236594068937" ID="Freemind_Link_809502424" MODIFIED="1236594071531" TEXT="pontos fracos">
+<node CREATED="1236594081062" ID="Freemind_Link_726137447" MODIFIED="1236594083296" TEXT="feio"/>
+<node CREATED="1236594084500" ID="Freemind_Link_1394969220" MODIFIED="1236594087281" TEXT="sem usabilidade"/>
+<node CREATED="1236594113609" ID="Freemind_Link_165363998" MODIFIED="1236594116265" TEXT="muita propaganda"/>
+<node CREATED="1236594118609" ID="Freemind_Link_16673705" MODIFIED="1236594122140" TEXT="n&#xe3;o &#xe9; colaborativo"/>
+<node CREATED="1236594252171" ID="Freemind_Link_1515004861" MODIFIED="1236595230359" TEXT="cheio de problemas (n&#xe3;o funciona)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1236594264078" ID="Freemind_Link_661237600" MODIFIED="1236594265000" TEXT="em asp"/>
+</node>
+</node>
+<node CREATED="1236594186578" ID="Freemind_Link_930968047" MODIFIED="1236594187515" TEXT="http://www.redeviagem.com/">
+<node CREATED="1236594190000" ID="Freemind_Link_1430921203" MODIFIED="1236594307187" TEXT="pontos fortes">
+<node CREATED="1236594282421" ID="Freemind_Link_1615675106" MODIFIED="1236594292000" TEXT="simples, mas bonito"/>
+<node CREATED="1236594309140" ID="Freemind_Link_153166502" MODIFIED="1236594319140" TEXT="&#xfa;ltimas atividades na p&#xe1;gina principal"/>
+<node CREATED="1236594339671" ID="Freemind_Link_1468551490" MODIFIED="1236594342406" TEXT="conceito de grupos"/>
+<node CREATED="1236594385203" ID="Freemind_Link_1963623582" MODIFIED="1236594387078" TEXT="f&#xf3;rum"/>
+<node CREATED="1236594712109" ID="Freemind_Link_1990836995" MODIFIED="1236594717250" TEXT="rss do f&#xf3;rum"/>
+<node CREATED="1236594728296" ID="Freemind_Link_1890368897" MODIFIED="1236594733890" TEXT="seguir discuss&#xe3;o no f&#xf3;rum"/>
+<node CREATED="1236594746828" ID="Freemind_Link_806972484" MODIFIED="1236594750921" TEXT="v&#xed;deos"/>
+</node>
+<node CREATED="1236594193812" ID="Freemind_Link_1631524478" MODIFIED="1236594195921" TEXT="pontos fracos">
+<node CREATED="1236594495109" ID="Freemind_Link_1106698463" MODIFIED="1236594497968" TEXT="n&#xe3;o tem mapa"/>
+<node CREATED="1236594499593" ID="Freemind_Link_1189337983" MODIFIED="1236594502250" TEXT="n&#xe3;o tem roteiro"/>
+<node CREATED="1236594504125" ID="Freemind_Link_483339400" MODIFIED="1236594527984" TEXT="n&#xe3;o tem muita utilidade, cadastro de pessoas, f&#xf3;rum e grupos (esse n&#xe3;o tem finalidade)"/>
+</node>
+</node>
+<node CREATED="1236594908562" ID="Freemind_Link_323836364" MODIFIED="1236594925750" TEXT="http://www.bluenity.com/ (EUA)">
+<node CREATED="1236594910234" ID="Freemind_Link_1251210942" MODIFIED="1236594912781" TEXT="pontos fortes">
+<node CREATED="1236594933546" ID="Freemind_Link_61583058" MODIFIED="1236594935796" TEXT="muito bonito"/>
+<node CREATED="1236594951453" ID="Freemind_Link_49300102" MODIFIED="1236594962234" TEXT="&#xfa;ltimas dicas na p&#xe1;gina principal"/>
+<node CREATED="1236595100484" ID="Freemind_Link_822765057" MODIFIED="1236595110937" TEXT="autocomplete no cadastro do pa&#xed;s, cidade"/>
+<node CREATED="1236595494953" ID="Freemind_Link_1203092913" MODIFIED="1236595496765" TEXT="mail box"/>
+<node CREATED="1236595511500" ID="Freemind_Link_1239234539" MODIFIED="1236595512718" TEXT="contatos"/>
+<node CREATED="1236595684437" ID="Freemind_Link_371458137" MODIFIED="1236595689218" TEXT="mapa de onde j&#xe1; passei"/>
+<node CREATED="1236595727578" ID="Freemind_Link_511541565" MODIFIED="1236595728578" TEXT="ajax"/>
+<node CREATED="1236595775109" ID="Freemind_Link_999019222" MODIFIED="1236595834062" TEXT="mostra o clima do lugar pesquisa (&#xb0;C e &#xb0;F)"/>
+<node CREATED="1236595999718" ID="Freemind_Link_704972364" MODIFIED="1236596019859" TEXT="profile da pessoa apresenta todos os lugares que a pessoa j&#xe1; passou(google maps)"/>
+<node CREATED="1236596031843" ID="Freemind_Link_690166891" MODIFIED="1236596044921" TEXT="no mapa dos lugares, destaca o &#xfa;ltimo"/>
+</node>
+<node CREATED="1236594913968" ID="Freemind_Link_1681238741" MODIFIED="1236595636234" TEXT="pontos fracos">
+<node CREATED="1236594992593" ID="Freemind_Link_165915133" MODIFIED="1236594999875" TEXT="java script pra dar e vender"/>
+<node CREATED="1236595025296" ID="Freemind_Link_167496502" MODIFIED="1236595032375" TEXT="s&#xf3; cadastrados visualizam conte&#xfa;do"/>
+<node CREATED="1236595627515" ID="Freemind_Link_184514989" MODIFIED="1236595648312" TEXT="n&#xe3;o se encontra ningu&#xe9;m no sistema"/>
+<node CREATED="1236595650625" ID="Freemind_Link_1310706705" MODIFIED="1236595660031" TEXT="n&#xe3;o da pra visualizar as outras pessoas, comunidades, etc"/>
+<node CREATED="1236595661234" ID="Freemind_Link_1598495703" MODIFIED="1236595682203" TEXT="s&#xf3; se forma grupos por convite, n&#xe3;o &#xe9; muito colaborativo"/>
+</node>
+</node>
+</node>
+<node CREATED="1236005874375" ID="Freemind_Link_1514993177" MODIFIED="1236594037515" TEXT="n&#xed;vel pra quem participa"/>
+<node CREATED="1236005914156" ID="Freemind_Link_653378091" MODIFIED="1236005920406" TEXT="indicar trechos bons e ruins"/>
+<node CREATED="1236005921609" ID="Freemind_Link_728668950" MODIFIED="1236005926078" TEXT="indicar lugares bons"/>
+<node CREATED="1236006573421" ID="Freemind_Link_1680157417" MODIFIED="1236007855734" TEXT="colaborativo">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1236007947546" ID="Freemind_Link_169414542" MODIFIED="1236007949234" TEXT="teoria"/>
+<node CREATED="1236007950437" ID="Freemind_Link_1006085086" MODIFIED="1236007954250" TEXT="pr&#xe1;tca"/>
+</node>
+<node CREATED="1236006650281" ID="Freemind_Link_1543514940" MODIFIED="1236006659828" TEXT="maplink"/>
+<node CREATED="1236006734953" ID="Freemind_Link_1360559932" MODIFIED="1236006736156" TEXT="n&#xe3;o depende de ningu&#xe9;m"/>
+<node CREATED="1236007843546" ID="Freemind_Link_1991448309" MODIFIED="1236007851593" TEXT="transito">
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1236008109421" ID="Freemind_Link_1268414018" MODIFIED="1236008116390" TEXT="na forma de servi&#xe7;o"/>
+<node CREATED="1236008134390" ID="Freemind_Link_258998863" MODIFIED="1236008137812" TEXT="multiplas interfaces"/>
+<node CREATED="1236008186515" ID="Freemind_Link_668306378" MODIFIED="1236008194375" TEXT="inclui os roteiros da sua viajem">
+<node CREATED="1236008270546" ID="Freemind_Link_1868103944" MODIFIED="1236008275406" TEXT="todos tem acesso aos roteiros"/>
+<node CREATED="1236008276578" ID="Freemind_Link_1462892376" MODIFIED="1236008283453" TEXT="iteressante para conhecer outros roteiros"/>
+<node CREATED="1236008285484" ID="Freemind_Link_269643138" MODIFIED="1236008289578" TEXT="v&#xe1;riedade de rotas"/>
+<node CREATED="1236008340453" ID="Freemind_Link_1515532942" MODIFIED="1236008344921" TEXT="impress&#xe3;o do roteiro">
+<node CREATED="1236008347640" ID="Freemind_Link_197933813" MODIFIED="1236008350109" TEXT="com gr&#xe1;ficos"/>
+<node CREATED="1236008550000" ID="Freemind_Link_1584022026" MODIFIED="1236008551140" TEXT="mapas"/>
+<node CREATED="1236008552703" ID="Freemind_Link_1388328611" MODIFIED="1236008555468" TEXT="rota"/>
+<node CREATED="1236008555859" ID="Freemind_Link_1748797456" MODIFIED="1236008557921" TEXT="eventos"/>
+<node CREATED="1236008560281" ID="Freemind_Link_510107524" MODIFIED="1236008561500" TEXT="custos"/>
+</node>
+<node CREATED="1236008386390" ID="Freemind_Link_1363887200" MODIFIED="1236008388109" TEXT="custos"/>
+</node>
+<node CREATED="1236594440281" ID="Freemind_Link_270568716" MODIFIED="1236594643906" TEXT="id&#xe9;ias">
+<node CREATED="1236594444625" ID="Freemind_Link_735724260" MODIFIED="1236594452906" TEXT="podiamos integrar com o picasa do google"/>
+<node CREATED="1236594647406" ID="Freemind_Link_651394401" MODIFIED="1236594654265" TEXT="blog, di&#xe1;rio do viajante"/>
+<node CREATED="1236594755203" ID="Freemind_Link_838373610" MODIFIED="1236594769562" TEXT="na cria&#xe7;&#xe3;o do roteiro, criar agenda no google calendar"/>
+<node CREATED="1236594770781" ID="Freemind_Link_1324532382" MODIFIED="1236594775921" TEXT="integrar com o youtube.com"/>
+<node CREATED="1236595842421" ID="Freemind_Link_889477781" MODIFIED="1236595847625" TEXT="webservice de tempo"/>
+</node>
+<node CREATED="1236613926390" ID="Freemind_Link_156561697" MODIFIED="1236614377562" TEXT="tema: montagem e avalia&#xe7;&#xe3;o de roteiros de viagens">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1236614175843" ID="Freemind_Link_372419917" MODIFIED="1236614179140" TEXT="fotos dos lugares">
+<node CREATED="1236614180265" ID="Freemind_Link_561398776" MODIFIED="1236614182796" TEXT="voto nas fotos"/>
+</node>
+<node CREATED="1236614306437" ID="Freemind_Link_289301485" MODIFIED="1236614310312" TEXT="estat&#xed;stica"/>
+</node>
+</node>
+<node CREATED="1236006561609" ID="Freemind_Link_201022610" MODIFIED="1236006568421" POSITION="left" TEXT="quest&#xf5;es">
+<node CREATED="1236006569453" ID="Freemind_Link_1642055842" MODIFIED="1236007840234" TEXT="colaborativo">
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1236006578203" ID="Freemind_Link_1011438241" MODIFIED="1236006582171" TEXT="qualquer disciplina"/>
+<node CREATED="1236006583453" ID="Freemind_Link_1428439899" MODIFIED="1236006586218" TEXT="estatisticas"/>
+<node CREATED="1236006587640" ID="Freemind_Link_1157821392" MODIFIED="1236006593796" TEXT="hist&#xf3;rico do usu&#xe1;rio"/>
+<node CREATED="1236006596984" ID="Freemind_Link_1834157650" MODIFIED="1236006599375" TEXT="simulados"/>
+<node CREATED="1236006601375" ID="Freemind_Link_522347399" MODIFIED="1236006608718" TEXT="classifica&#xe7;&#xe3;o das quest&#xf5;es pelo n&#xb0; de acertos"/>
+<node CREATED="1236006677515" ID="Freemind_Link_1549475523" MODIFIED="1236006681703" TEXT="questoesDeConcursos"/>
+<node CREATED="1236006718078" ID="Freemind_Link_86257225" MODIFIED="1236006724578" TEXT="n&#xe3;o depende de ningu&#xe9;m"/>
+</node>
+<node CREATED="1236006612203" ID="Freemind_Link_1943762432" MODIFIED="1236006617843" POSITION="right" TEXT="prontuario m&#xe9;dico">
+<node CREATED="1236006620250" ID="Freemind_Link_1051901830" MODIFIED="1236006625062" TEXT="integra&#xe7;&#xe3;o"/>
+<node CREATED="1236006626437" ID="Freemind_Link_1729532481" MODIFIED="1236006629078" TEXT="web services"/>
+<node CREATED="1236006630703" ID="Freemind_Link_1986980832" MODIFIED="1236006633937" TEXT="middleware"/>
+<node CREATED="1236006634968" ID="Freemind_Link_566604621" MODIFIED="1236006643609" TEXT="algu&#xe9;m da &#xe1;rea m&#xe9;dica fornecendo requisitos"/>
+<node CREATED="1236007135718" ID="Freemind_Link_1734021995" MODIFIED="1236007879656" TEXT="dependente de algu&#xe9;m">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1236006860468" ID="Freemind_Link_607195967" MODIFIED="1236007810234" POSITION="left" TEXT="musicArch">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1236006866031" ID="Freemind_Link_1611124055" MODIFIED="1236006870031" TEXT="ningu&#xe9;m ta afim"/>
+<node CREATED="1236006871359" ID="Freemind_Link_1415969950" MODIFIED="1236006876343" TEXT="analise das m&#xfa;sicas"/>
+<node CREATED="1236006878078" ID="Freemind_Link_898078811" MODIFIED="1236006878921" TEXT="etc"/>
+</node>
+<node CREATED="1236007282203" ID="Freemind_Link_1225287008" MODIFIED="1236007888203" POSITION="right" TEXT="id&#xe9;ias dos outros">
+<icon BUILTIN="help"/>
+<node CREATED="1236007286765" ID="Freemind_Link_1951173007" MODIFIED="1236007293781" TEXT="sistema de bonifica&#xe7;&#xe3;o"/>
+<node CREATED="1236007303671" ID="Freemind_Link_1826874473" MODIFIED="1236007382296" TEXT="compartilhamento de m&#xfa;sicas via mobile (emule mobile)"/>
+<node CREATED="1236007496031" ID="Freemind_Link_1598406974" MODIFIED="1236007508500" TEXT="verificador de &#xe1;reas de interesse (mobile)"/>
+<node CREATED="1236007515796" ID="Freemind_Link_1527405466" MODIFIED="1236007537843" TEXT="acessibilidade (para deficientes) - pesquisa aplicada"/>
+</node>
+<node CREATED="1236007718234" ID="Freemind_Link_768579720" MODIFIED="1236007928500" POSITION="left" TEXT="banco de horas">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1236007721906" ID="Freemind_Link_1586607414" MODIFIED="1236007725125" TEXT="sistema sistema"/>
+<node CREATED="1236007726125" ID="Freemind_Link_1338451790" MODIFIED="1236007729140" TEXT="ningu&#xe9;m ta afim"/>
+<node CREATED="1236007730359" ID="Freemind_Link_727459548" MODIFIED="1236007731531" TEXT="etc"/>
+</node>
+</node>
+</map>
