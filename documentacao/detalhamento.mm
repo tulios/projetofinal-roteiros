6365cabe02ebf8daa71e954a64f8c88e72a9ba18
tulios <ornelas.tulio@gmail.com>
2009-04-01 11:07:33 -0300

    Adicao do detalhamento e proposta (termo de aceite) + separacao das ideias em outro mapa mental

---
+++ documentacao/detalhamento.mm
@@ -0,0 +1,27 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1238591094062" ID="Freemind_Link_1072743693" MODIFIED="1238591101156" TEXT="roteiros">
+<node CREATED="1238591102718" ID="_" MODIFIED="1238591160796" POSITION="right" TEXT="nome">
+<node CREATED="1238591162140" ID="Freemind_Link_319055811" MODIFIED="1238591162843" TEXT="?"/>
+<node CREATED="1238591472343" ID="Freemind_Link_791503891" MODIFIED="1238591478468" TEXT="palavras-chave">
+<node CREATED="1238591479218" ID="Freemind_Link_961972868" MODIFIED="1238591480671" TEXT="turismo"/>
+<node CREATED="1238591482312" ID="Freemind_Link_1656774183" MODIFIED="1238591485453" TEXT="rota"/>
+<node CREATED="1238591488781" ID="Freemind_Link_1708000213" MODIFIED="1238591494218" TEXT="rede de informa&#xe7;&#xf5;es"/>
+<node CREATED="1238591495468" ID="Freemind_Link_1170879557" MODIFIED="1238591513093" TEXT="viagem"/>
+<node CREATED="1238591562046" ID="Freemind_Link_621653778" MODIFIED="1238591566343" TEXT="colabora&#xe7;&#xe3;o"/>
+</node>
+</node>
+<node CREATED="1238591305781" ID="Freemind_Link_1056102154" MODIFIED="1238591328203" POSITION="left" TEXT="&#xe1;rea de abrang&#xea;ncia">
+<node CREATED="1238591329218" ID="Freemind_Link_775784456" MODIFIED="1238591383218" TEXT="Rede colaborativa de informa&#xe7;&#xf5;es sobre turismo">
+<node CREATED="1238591663609" ID="Freemind_Link_1793364537" MODIFIED="1238591664828" TEXT="dicas"/>
+<node CREATED="1238591668437" ID="Freemind_Link_1850823669" MODIFIED="1238591670687" TEXT="opni&#xf5;es"/>
+</node>
+<node CREATED="1238591366421" ID="Freemind_Link_910927990" MODIFIED="1238591393578" TEXT="Auxilio na cria&#xe7;&#xe3;o de roteiros de viagens"/>
+<node CREATED="1238591417281" ID="Freemind_Link_455100908" MODIFIED="1238591429484" TEXT="Favorece a divulga&#xe7;&#xe3;o de pontos turisticos"/>
+<node CREATED="1238591613281" ID="Freemind_Link_1380230242" MODIFIED="1238591625515" TEXT="Avalia&#xe7;&#xe3;o dos pontos turisticos e estabelecimentos visitados"/>
+<node CREATED="1238591781765" ID="Freemind_Link_1506097395" MODIFIED="1238591791328" TEXT="Hist&#xf3;rico das viagens do usu&#xe1;rio"/>
+<node CREATED="1238591804937" ID="Freemind_Link_846217849" MODIFIED="1238591813000" TEXT="Sugest&#xf5;es de roteiros"/>
+<node CREATED="1238591912500" ID="Freemind_Link_1758317856" MODIFIED="1238591915984" TEXT="Eventos das cidades"/>
+</node>
+</node>
+</map>
