a5802522b07b84ca68993e36b86375b443ff86e9
unknown <Ricardo@.(none)>
2009-04-09 11:10:44 -0300

    adicionando braimstorm da geracao do diagrama de dominio do mapa mental

--- documentacao/detalhamento.mm
+++ documentacao/detalhamento.mm
@@ -17,11 +17,33 @@
 <node CREATED="1238591668437" ID="Freemind_Link_1850823669" MODIFIED="1238591670687" TEXT="opni&#xf5;es"/>
 </node>
 <node CREATED="1238591366421" ID="Freemind_Link_910927990" MODIFIED="1238591393578" TEXT="Auxilio na cria&#xe7;&#xe3;o de roteiros de viagens"/>
-<node CREATED="1238591417281" ID="Freemind_Link_455100908" MODIFIED="1238591429484" TEXT="Favorece a divulga&#xe7;&#xe3;o de pontos turisticos"/>
+<node CREATED="1238591417281" ID="Freemind_Link_455100908" MODIFIED="1239285251953" TEXT="Favorece a divulga&#xe7;&#xe3;o de pontos turisticos"/>
 <node CREATED="1238591613281" ID="Freemind_Link_1380230242" MODIFIED="1238591625515" TEXT="Avalia&#xe7;&#xe3;o dos pontos turisticos e estabelecimentos visitados"/>
 <node CREATED="1238591781765" ID="Freemind_Link_1506097395" MODIFIED="1238591791328" TEXT="Hist&#xf3;rico das viagens do usu&#xe1;rio"/>
 <node CREATED="1238591804937" ID="Freemind_Link_846217849" MODIFIED="1238591813000" TEXT="Sugest&#xf5;es de roteiros"/>
 <node CREATED="1238591912500" ID="Freemind_Link_1758317856" MODIFIED="1238591915984" TEXT="Eventos das cidades"/>
+<node CREATED="1239285204171" ID="Freemind_Link_1792574745" MODIFIED="1239285217156" TEXT="compartilhamento de m&#xed;dia">
+<node CREATED="1239285226125" ID="Freemind_Link_1943442423" MODIFIED="1239285230609" TEXT="albuns"/>
+<node CREATED="1239285231968" ID="Freemind_Link_1838204908" MODIFIED="1239285235406" TEXT="fotos"/>
+</node>
+</node>
+<node CREATED="1239284965453" ID="Freemind_Link_867642423" MODIFIED="1239284970312" POSITION="right" TEXT="brainstorm">
+<node CREATED="1239284988000" ID="Freemind_Link_1313813371" MODIFIED="1239285082359" TEXT="ponto tur&#xed;stico"/>
+<node CREATED="1239285002828" ID="Freemind_Link_1184074315" MODIFIED="1239285085359" TEXT="evento"/>
+<node CREATED="1239285015515" ID="Freemind_Link_1050792327" MODIFIED="1239285132953" TEXT="roteiro"/>
+<node CREATED="1239285025687" ID="Freemind_Link_1034584664" MODIFIED="1239285026890" TEXT="passeio"/>
+<node CREATED="1239285027656" ID="Freemind_Link_727894428" MODIFIED="1239285058218" TEXT="cidade"/>
+<node CREATED="1239285058578" ID="Freemind_Link_1996750241" MODIFIED="1239285095093" TEXT="estabelecimento"/>
+<node CREATED="1239285097343" ID="Freemind_Link_1986140706" MODIFIED="1239285123296" TEXT="estado"/>
+<node CREATED="1239285160046" ID="Freemind_Link_761204249" MODIFIED="1239285161921" TEXT="viajante"/>
+<node CREATED="1239285178718" ID="Freemind_Link_1688106412" MODIFIED="1239285179671" TEXT="custo"/>
+<node CREATED="1239285256046" ID="Freemind_Link_1779027241" MODIFIED="1239285257281" TEXT="viagem"/>
+<node CREATED="1239285271625" ID="Freemind_Link_84648694" MODIFIED="1239285416859" TEXT="meio de transporte"/>
+<node CREATED="1239285280093" ID="Freemind_Link_767059071" MODIFIED="1239285338609" TEXT="dica"/>
+<node CREATED="1239285350218" ID="Freemind_Link_966370747" MODIFIED="1239285355250" TEXT="tipo de estabelecimento"/>
+<node CREATED="1239285375109" ID="Freemind_Link_137449608" MODIFIED="1239285421828" TEXT="avalia&#xe7;&#xe3;o">
+<icon BUILTIN="messagebox_warning"/>
+</node>
 </node>
 </node>
 </map>
