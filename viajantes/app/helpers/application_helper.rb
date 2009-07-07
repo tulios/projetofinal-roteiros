344d5ac51dc2cead8061dc35448cdaa0b25fca0c
tulios <ornelas.tulio@gmail.com>
2009-07-06 21:18:15 -0300

    Melhorias no design e no css.

--- viajantes/app/helpers/application_helper.rb
+++ viajantes/app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-	
+	def show_success()
+     if flash[:notice]
+       %Q{
+          <p class="success">#{flash[:notice]}</p>
+        }
+     end
+   end
 end
