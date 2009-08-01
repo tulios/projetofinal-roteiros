a8323a0574283393b1c926ff324f42b9f68d3033
tulios <ornelas.tulio@gmail.com>
2009-07-31 23:47:16 -0300

    Corrigido bug: agora quando se utiliza a paginacao na pagina de pesquisa mantem-se a aba selecionada.

--- viajantes/test/functional/search_controller_test.rb
+++ viajantes/test/functional/search_controller_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Cria 21 estabelecimentos e 21 pontos turisticos" do
+		
+		city_id = cities(:one).to_param
+		city = City.find(city_id)
+
+		for x in (0..51)
+			conteudo = {:name => "ACHE_TS#{x}", :address => "ADS#{x}", :description => "desc#{x}", :city => city}
+			ts = TouristSight.new(conteudo)
+			ts.save
+		end
+
+		for x in (0..51)
+			conteudo = {:name => "ACHE_SHOP#{x}", :address => "SHOP#{x}", :city => city}
+			shop = Shop.new(conteudo)
+			shop.save
+		end
   end
 end
