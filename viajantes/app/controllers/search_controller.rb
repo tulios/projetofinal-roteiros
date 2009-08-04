19902c6addc1df5f3e194e27a62395b98a673c58
tulios <ornelas.tulio@gmail.com>
2009-08-04 10:45:58 -0300

    Testes para cities_controller e search_controller

--- viajantes/app/controllers/search_controller.rb
+++ viajantes/app/controllers/search_controller.rb
@@ -2,8 +2,8 @@ class SearchController < ApplicationController
 
 	def index
 		@value = params[:value]
-		@shops = Shop.find_like_name(@value, 10, params[:page_shop])
-		@tourist_sights = TouristSight.find_like_name_or_key_word(@value, 10, params[:page_ts])
+		@shops = Shop.find_like_name_or_key_word(@value, 10, params[:page_shop])
+		@tourist_sights = TouristSight.find_like_name(@value, 10, params[:page_ts])
 		
 		@results = ((@shops and @shops.length > 0) or (@tourist_sights and @tourist_sights.length > 0))
 		if params[:selected]
--- viajantes/test/functional/search_controller_test.rb
+++ viajantes/test/functional/search_controller_test.rb
@@ -1,22 +1,76 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "Cria 21 estabelecimentos e 21 pontos turisticos" do
-		
-		city_id = cities(:one).to_param
-		city = City.find(city_id)
 
-		for x in (0..51)
-			conteudo = {:name => "ACHE_TS#{x}", :address => "ADS#{x}", :description => "desc#{x}", :city => city}
-			ts = TouristSight.new(conteudo)
-			ts.save
-		end
+  test "Deveria recuperar pontos turisticos e estabelecimentos pelo nome ou palavra chave" do
 
-		for x in (0..51)
-			conteudo = {:name => "ACHE_SHOP#{x}", :address => "SHOP#{x}", :city => city}
-			shop = Shop.new(conteudo)
-			shop.save
-		end
+    get :index, :value => "o"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(2,assigns(:tourist_sights).length)
+    assert_equal(2,assigns(:shops).length)
+    assert_equal(true,assigns(:results))
+  end
+
+  test "Deveria recuperar estabelecimentos pelo nome ou palavra chave" do
+
+    get :index, :value => "sHoP"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(0,assigns(:tourist_sights).length)
+    assert_equal(2,assigns(:shops).length)
+    assert_equal(true,assigns(:results))
+
+    get :index, :value => "sHoP-1"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(0,assigns(:tourist_sights).length)
+    assert_equal(1,assigns(:shops).length)
+    assert_equal(true,assigns(:results))
+  end
+
+  test "Deveria recuperar pontos turisticos pelo nome" do
+
+    get :index, :value => "pOnTo"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(2,assigns(:tourist_sights).length)
+    assert_equal(0,assigns(:shops).length)
+    assert_equal(true,assigns(:results))
+
+    get :index, :value => "PoNtOtUrIsTo1"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(1,assigns(:tourist_sights).length)
+    assert_equal(0,assigns(:shops).length)
+    assert_equal(true,assigns(:results))
+  end
+
+  test "Deveria nao encontrar nenhum resultado" do
+
+    get :index, :value => "texto_nao_existente"
+    assert_response :success
+
+    assert_not_nil(assigns(:tourist_sights))
+    assert_not_nil(assigns(:shops))
+
+    assert_equal(0,assigns(:tourist_sights).length)
+    assert_equal(0,assigns(:shops).length)
+    assert_equal(false,assigns(:results))
   end
 end
