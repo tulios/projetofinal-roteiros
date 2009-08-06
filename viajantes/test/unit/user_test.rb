40bb1f4694429ed43ba561a5cbdd98f12a00c322
tulios <ornelas.tulio@gmail.com>
2009-08-06 11:17:01 -0300

    Correcao da maioria dos testes relacionados a usuario. Ainda existem 4 faltando.

--- viajantes/test/unit/user_test.rb
+++ viajantes/test/unit/user_test.rb
@@ -1,73 +1,72 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class UserTest < Test::Unit::TestCase
+class UserTest < ActiveSupport::TestCase
   # Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead.
   # Then, you can remove it from this and the functional test.
-  include AuthenticatedTestHelper
 
-  def test_should_create_user
+  test "Deveria criar um usuario" do
     assert_difference 'User.count' do
       user = create_user
       assert !user.new_record?, "#{user.errors.full_messages.to_sentence}"
     end
   end
 
-  def test_should_require_login
+  test "Deveria obrigar a preencher o login" do
     assert_no_difference 'User.count' do
       u = create_user(:login => nil)
       assert u.errors.on(:login)
     end
   end
 
-  def test_should_require_password
+  test "Deveria obrigar a preencher a senha" do
     assert_no_difference 'User.count' do
       u = create_user(:password => nil)
       assert u.errors.on(:password)
     end
   end
 
-  def test_should_require_password_confirmation
+  test "Deveria obrigar a preencher a confirmacao da senha" do
     assert_no_difference 'User.count' do
       u = create_user(:password_confirmation => nil)
       assert u.errors.on(:password_confirmation)
     end
   end
 
-  def test_should_require_email
+  test "Deveria obrigar a preencher o email" do
     assert_no_difference 'User.count' do
       u = create_user(:email => nil)
       assert u.errors.on(:email)
     end
   end
 
-  def test_should_reset_password
+  test "Deveria resetar a senha" do
     users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
     assert_equal users(:quentin), User.authenticate('quentin', 'new password')
   end
 
-  def test_should_not_rehash_password
+  test "Deveria nao trocar o hash da senha" do
     users(:quentin).update_attributes(:login => 'quentin2')
     assert_equal users(:quentin), User.authenticate('quentin2', 'test')
   end
 
-  def test_should_authenticate_user
+  test "Deveria autenticar o usuario" do
     assert_equal users(:quentin), User.authenticate('quentin', 'test')
   end
 
-  def test_should_set_remember_token
+  test "Deveria ligar o token de lembrar" do
     users(:quentin).remember_me
     assert_not_nil users(:quentin).remember_token
     assert_not_nil users(:quentin).remember_token_expires_at
   end
 
-  def test_should_unset_remember_token
+  test "Deveria desligar o token de lembrar" do
     users(:quentin).remember_me
     assert_not_nil users(:quentin).remember_token
     users(:quentin).forget_me
     assert_nil users(:quentin).remember_token
   end
 
-  def test_should_remember_me_for_one_week
+  test "Deveria lembrar do usuario por 1 semana" do
     before = 1.week.from_now.utc
     users(:quentin).remember_me_for 1.week
     after = 1.week.from_now.utc
@@ -76,7 +75,7 @@ class UserTest < Test::Unit::TestCase
     assert users(:quentin).remember_token_expires_at.between?(before, after)
   end
 
-  def test_should_remember_me_until_one_week
+  test "Deveria lembrar do usuario ate 1 semana" do
     time = 1.week.from_now.utc
     users(:quentin).remember_me_until time
     assert_not_nil users(:quentin).remember_token
@@ -84,7 +83,7 @@ class UserTest < Test::Unit::TestCase
     assert_equal users(:quentin).remember_token_expires_at, time
   end
 
-  def test_should_remember_me_default_two_weeks
+  test "Deveria lembrar do usuario por 2 semanas, valor padrao" do
     before = 2.weeks.from_now.utc
     users(:quentin).remember_me
     after = 2.weeks.from_now.utc
