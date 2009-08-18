e88045273450cff3ca9564033dbfbc606ff1a3b7
Pedro Capaça <pedro.capaca@gmail.com>
2009-08-18 20:34:39 -0300

    Voltando o destination.rb como estava

--- viajantes/app/models/destination.rb
+++ viajantes/app/models/destination.rb
@@ -10,9 +10,19 @@ class Destination < ActiveRecord::Base
   belongs_to :roadmap
   belongs_to :vehicle
   belongs_to :city
-
-	has_many :programs
   
-  validates_presence_of :roadmap, :city
+  has_many :programs
+  
+  validates_presence_of :roadmap, :city_id
+  validates_presence_of :start_date, :if => "not end_date.nil?"
+  validates_presence_of :end_date, :if => "not start_date.nil?"
+  
+  def validate
+    errors.add(:end_date, "deve ser antes da data se início.") if end_date < start_date
+  end 
+  
+  def number_of_days
+    end_date - start_date
+  end
+  
 end
-
