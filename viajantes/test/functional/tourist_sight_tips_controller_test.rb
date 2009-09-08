b7447508c7794c61ddd44ac15d32b981a571ef2c
Pedro Capaça <pedro.capaca@gmail.com>
2009-09-08 11:38:24 -0300

    Iniciados os testes funcionais de tourist_sight_tips

--- viajantes/test/functional/tourist_sight_tips_controller_test.rb
+++ viajantes/test/functional/tourist_sight_tips_controller_test.rb
@@ -1,8 +1,40 @@
 require 'test_helper'
 
 class TouristSightTipsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+ 
+  test "Deveria carregar o new" do
+    login_as :quentin
+    
+    get :new, :tourist_sight_id => tourist_sights(:one).to_param
+    assert_response :success
+    
+    assert(assigns(:tourist_sight))
+    assert(assigns(:tourist_sight_tip))
+    assert(assigns(:tourist_sight_tip).tip)
   end
+  
+  test "Deveria criar uma nova dica de ponto turístico" do
+    login_as :quentin
+  
+    assert_difference('TouristSightTip.count and Tip.count') do
+      create_tourist_sight_tip(roadmaps(:one).to_param)
+      assert(assigns(:tip))
+      assert(assigns(:tourist_sight_tip))
+      assert_response :redirect
+    end
+
+    assert_redirected_to tourist_sight_path(assigns(:tourist_sight_tip).tourist_sight)
+  end
+  
+  protected
+  
+  def create_tourist_sight_tip(tourist_sight_id, options = {})
+    tip_hash = {
+      :name => "NomeDaDica",
+      :description => "DescricaoDaDica",
+    }
+    
+    post :create, :tourist_sight_id => tourist_sight_id, :tip => tip_hash.merge(options)
+  end
+ 
 end
