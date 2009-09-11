c2568d67fdea2d4b0d15479fdd4a3bbf36cb9662
tulios <ornelas.tulio@gmail.com>
2009-09-10 21:56:51 -0300

    Alguns ajustes na pagina inicial.

--- documentacao/MapasMentais/detalhamento.mm
+++ documentacao/MapasMentais/detalhamento.mm
@@ -1,73 +1,80 @@
-<map version="0.7.1">
-<node ID="_Freemind_Link_1072743693" TEXT="roteiros">
-<node ID="_" TEXT="nome" POSITION="right">
-<node ID="_Freemind_Link_319055811" TEXT="?"/>
-<node ID="_Freemind_Link_791503891" TEXT="palavras-chave">
-<node ID="_Freemind_Link_961972868" TEXT="turismo"/>
-<node ID="_Freemind_Link_1656774183" TEXT="rota"/>
-<node ID="_Freemind_Link_1708000213" TEXT="rede de informa&#xe7;&#xf5;es"/>
-<node ID="_Freemind_Link_1170879557" TEXT="viagem"/>
-<node ID="_Freemind_Link_621653778" TEXT="colabora&#xe7;&#xe3;o"/>
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1252582948944" ID="_Freemind_Link_1072743693" MODIFIED="1252582948944" TEXT="roteiros">
+<node CREATED="1252582948944" ID="_" MODIFIED="1252582948944" POSITION="right" TEXT="nome">
+<node CREATED="1252582948944" ID="_Freemind_Link_319055811" MODIFIED="1252583114905" TEXT="?" VSHIFT="-86">
+<node CREATED="1252582966746" ID="Freemind_Link_1198300908" MODIFIED="1252582974184" TEXT="viajeabessa"/>
+<node CREATED="1252582976053" ID="Freemind_Link_1617480770" MODIFIED="1252583171759" TEXT="wikitravel"/>
+<node CREATED="1252583175449" ID="Freemind_Link_87625534" MODIFIED="1252583272811" TEXT="facasuamala"/>
+<node CREATED="1252583406638" ID="Freemind_Link_89205910" MODIFIED="1252583408982" TEXT="viaticum"/>
+<node CREATED="1252584987001" ID="Freemind_Link_1530784932" MODIFIED="1252584996401" TEXT="queroviajarbem"/>
+</node>
+<node CREATED="1252582948944" ID="_Freemind_Link_791503891" MODIFIED="1252582948944" TEXT="palavras-chave">
+<node CREATED="1252582948944" ID="_Freemind_Link_961972868" MODIFIED="1252582948944" TEXT="turismo"/>
+<node CREATED="1252582948945" ID="_Freemind_Link_1656774183" MODIFIED="1252582948945" TEXT="rota"/>
+<node CREATED="1252582948945" ID="_Freemind_Link_1708000213" MODIFIED="1252582948945" TEXT="rede de informa&#xe7;&#xf5;es"/>
+<node CREATED="1252582948945" ID="_Freemind_Link_1170879557" MODIFIED="1252582948945" TEXT="viagem"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_621653778" MODIFIED="1252582948946" TEXT="colabora&#xe7;&#xe3;o"/>
 </node>
 </node>
-<node ID="_Freemind_Link_1056102154" TEXT="&#xe1;rea de abrang&#xea;ncia" POSITION="left">
-<node ID="_Freemind_Link_775784456" TEXT="Rede colaborativa de informa&#xe7;&#xf5;es sobre turismo">
-<node ID="_Freemind_Link_1793364537" TEXT="dicas"/>
-<node ID="_Freemind_Link_1850823669" TEXT="opni&#xf5;es"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1056102154" MODIFIED="1252582948946" POSITION="left" TEXT="&#xe1;rea de abrang&#xea;ncia">
+<node CREATED="1252582948946" ID="_Freemind_Link_775784456" MODIFIED="1252582948946" TEXT="Rede colaborativa de informa&#xe7;&#xf5;es sobre turismo">
+<node CREATED="1252582948946" ID="_Freemind_Link_1793364537" MODIFIED="1252582948946" TEXT="dicas"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1850823669" MODIFIED="1252582948946" TEXT="opni&#xf5;es"/>
 </node>
-<node ID="_Freemind_Link_910927990" TEXT="Auxilio na cria&#xe7;&#xe3;o de roteiros de viagens"/>
-<node ID="_Freemind_Link_455100908" TEXT="Favorece a divulga&#xe7;&#xe3;o de pontos turisticos"/>
-<node ID="_Freemind_Link_1380230242" TEXT="Avalia&#xe7;&#xe3;o dos pontos turisticos e estabelecimentos visitados"/>
-<node ID="_Freemind_Link_1506097395" TEXT="Hist&#xf3;rico das viagens do usu&#xe1;rio"/>
-<node ID="_Freemind_Link_846217849" TEXT="Sugest&#xf5;es de roteiros"/>
-<node ID="_Freemind_Link_1758317856" TEXT="Eventos das cidades"/>
-<node ID="_Freemind_Link_1792574745" TEXT="compartilhamento de m&#xed;dia">
-<node ID="_Freemind_Link_1943442423" TEXT="albuns"/>
-<node ID="_Freemind_Link_1838204908" TEXT="fotos"/>
-</node>
-</node>
-<node ID="_Freemind_Link_867642423" TEXT="brainstorm" POSITION="right">
-<node ID="_Freemind_Link_1313813371" TEXT="ponto tur&#xed;stico">
-<node ID="_Freemind_Link_1445719084" TEXT="Poder votar na qualidade de:">
-<node ID="_Freemind_Link_1037184303" TEXT="conforto"/>
-<node ID="_Freemind_Link_730059106" TEXT="limpeza"/>
-<node ID="_Freemind_Link_1008655205" TEXT="servi&#xe7;o"/>
-<node ID="_Freemind_Link_914489944" TEXT="qualidade/pre&#xe7;o"/>
-<node ID="_Freemind_Link_1861245238" TEXT="localiza&#xe7;&#xe3;o"/>
-<node ID="_Freemind_Link_1061605557" TEXT="restaurantes"/>
-<node TEXT="infraestrutura"/>
-<node TEXT="atendimento"/>
-<node TEXT="alimenta&#xe7;&#xe3;o"/>
-<node TEXT="geral"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_910927990" MODIFIED="1252582948946" TEXT="Auxilio na cria&#xe7;&#xe3;o de roteiros de viagens"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_455100908" MODIFIED="1252582948946" TEXT="Favorece a divulga&#xe7;&#xe3;o de pontos turisticos"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1380230242" MODIFIED="1252582948946" TEXT="Avalia&#xe7;&#xe3;o dos pontos turisticos e estabelecimentos visitados"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1506097395" MODIFIED="1252582948946" TEXT="Hist&#xf3;rico das viagens do usu&#xe1;rio"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_846217849" MODIFIED="1252582948946" TEXT="Sugest&#xf5;es de roteiros"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1758317856" MODIFIED="1252582948946" TEXT="Eventos das cidades"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1792574745" MODIFIED="1252582948946" TEXT="compartilhamento de m&#xed;dia">
+<node CREATED="1252582948946" ID="_Freemind_Link_1943442423" MODIFIED="1252582948946" TEXT="albuns"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_1838204908" MODIFIED="1252582948946" TEXT="fotos"/>
 </node>
 </node>
-<node ID="_Freemind_Link_1184074315" TEXT="evento"/>
-<node ID="_Freemind_Link_1050792327" TEXT="roteiro"/>
-<node ID="_Freemind_Link_1034584664" TEXT="passeio"/>
-<node ID="_Freemind_Link_727894428" TEXT="cidade"/>
-<node ID="_Freemind_Link_1996750241" TEXT="estabelecimento"/>
-<node ID="_Freemind_Link_1986140706" TEXT="estado"/>
-<node ID="_Freemind_Link_761204249" TEXT="viajante"/>
-<node ID="_Freemind_Link_1688106412" TEXT="custo"/>
-<node ID="_Freemind_Link_1779027241" TEXT="viagem"/>
-<node ID="_Freemind_Link_84648694" TEXT="meio de transporte"/>
-<node ID="_Freemind_Link_767059071" TEXT="dica"/>
-<node ID="_Freemind_Link_966370747" TEXT="tipo de estabelecimento"/>
-<node ID="_Freemind_Link_137449608" TEXT="avalia&#xe7;&#xe3;o">
+<node CREATED="1252582948946" ID="_Freemind_Link_867642423" MODIFIED="1252582948946" POSITION="right" TEXT="brainstorm">
+<node CREATED="1252582948946" ID="_Freemind_Link_1313813371" MODIFIED="1252582948946" TEXT="ponto tur&#xed;stico">
+<node CREATED="1252582948946" ID="_Freemind_Link_1445719084" MODIFIED="1252582948946" TEXT="Poder votar na qualidade de:">
+<node CREATED="1252582948946" ID="_Freemind_Link_1037184303" MODIFIED="1252582948946" TEXT="conforto"/>
+<node CREATED="1252582948946" ID="_Freemind_Link_730059106" MODIFIED="1252582948946" TEXT="limpeza"/>
+<node CREATED="1252582948947" ID="_Freemind_Link_1008655205" MODIFIED="1252582948947" TEXT="servi&#xe7;o"/>
+<node CREATED="1252582948947" ID="_Freemind_Link_914489944" MODIFIED="1252582948947" TEXT="qualidade/pre&#xe7;o"/>
+<node CREATED="1252582948947" ID="_Freemind_Link_1861245238" MODIFIED="1252582948947" TEXT="localiza&#xe7;&#xe3;o"/>
+<node CREATED="1252582948947" ID="_Freemind_Link_1061605557" MODIFIED="1252582948947" TEXT="restaurantes"/>
+<node CREATED="1252582948947" MODIFIED="1252582948947" TEXT="infraestrutura"/>
+<node CREATED="1252582948947" MODIFIED="1252582948947" TEXT="atendimento"/>
+<node CREATED="1252582948947" MODIFIED="1252582948947" TEXT="alimenta&#xe7;&#xe3;o"/>
+<node CREATED="1252582948947" MODIFIED="1252582948947" TEXT="geral"/>
+</node>
+</node>
+<node CREATED="1252582948947" ID="_Freemind_Link_1184074315" MODIFIED="1252582948947" TEXT="evento"/>
+<node CREATED="1252582948947" ID="_Freemind_Link_1050792327" MODIFIED="1252582948947" TEXT="roteiro"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1034584664" MODIFIED="1252582948948" TEXT="passeio"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_727894428" MODIFIED="1252582948948" TEXT="cidade"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1996750241" MODIFIED="1252582948948" TEXT="estabelecimento"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1986140706" MODIFIED="1252582948948" TEXT="estado"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_761204249" MODIFIED="1252582948948" TEXT="viajante"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1688106412" MODIFIED="1252582948948" TEXT="custo"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1779027241" MODIFIED="1252582948948" TEXT="viagem"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_84648694" MODIFIED="1252582948948" TEXT="meio de transporte"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_767059071" MODIFIED="1252582948948" TEXT="dica"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_966370747" MODIFIED="1252582948948" TEXT="tipo de estabelecimento"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_137449608" MODIFIED="1252582948948" TEXT="avalia&#xe7;&#xe3;o">
 <icon BUILTIN="messagebox_warning"/>
 </node>
 </node>
-<node ID="_Freemind_Link_1575818030" TEXT="Estilo (tag)" POSITION="left">
-<node ID="_Freemind_Link_1433250840" TEXT="Aventura"/>
-<node ID="_Freemind_Link_1225603298" TEXT="Rom&#xe2;ntico"/>
-<node ID="_Freemind_Link_1908630777" TEXT="Gastronomia"/>
-<node ID="_Freemind_Link_1779074746" TEXT="Ex&#xf3;tico"/>
-<node ID="_Freemind_Link_1298686284" TEXT="Ecoturismo"/>
-<node ID="_Freemind_Link_1338238645" TEXT="Neg&#xf3;cios"/>
-<node ID="_Freemind_Link_367919401" TEXT="Relax"/>
-<node ID="_Freemind_Link_697197067" TEXT="Cursos"/>
-<node ID="_Freemind_Link_629633989" TEXT="Compras"/>
-<node ID="_Freemind_Link_767997776" TEXT="Cultural"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1575818030" MODIFIED="1252582948948" POSITION="left" TEXT="Estilo (tag)">
+<node CREATED="1252582948948" ID="_Freemind_Link_1433250840" MODIFIED="1252582948948" TEXT="Aventura"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1225603298" MODIFIED="1252582948948" TEXT="Rom&#xe2;ntico"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1908630777" MODIFIED="1252582948948" TEXT="Gastronomia"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1779074746" MODIFIED="1252582948948" TEXT="Ex&#xf3;tico"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1298686284" MODIFIED="1252582948948" TEXT="Ecoturismo"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_1338238645" MODIFIED="1252582948948" TEXT="Neg&#xf3;cios"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_367919401" MODIFIED="1252582948948" TEXT="Relax"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_697197067" MODIFIED="1252582948948" TEXT="Cursos"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_629633989" MODIFIED="1252582948948" TEXT="Compras"/>
+<node CREATED="1252582948948" ID="_Freemind_Link_767997776" MODIFIED="1252582948948" TEXT="Cultural"/>
 </node>
 </node>
 </map>
