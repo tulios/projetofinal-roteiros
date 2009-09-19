b97e8a20bef3ac08aa987655bebae3ac64a5298f
tulios <ornelas.tulio@gmail.com>
2009-09-18 23:42:47 -0300

    Refactoring da tarefa db:scripts:load, agora ela utiliza o data source com as configuracoes definidas para o banco de dados. Ficou mais rapido tambem, pois nao utiliza outro processo para executar as querys.

--- viajantes/db/scripts/scripts.rb
+++ viajantes/db/scripts/scripts.rb
@@ -11,7 +11,7 @@ module Scripts
 		puts "Scripts:"
 		Dir.entries(SCRIPTS_DIR).sort.each do |file|
 			if sql?(file)
-				self.execute_postgres_script!(file)
+				self.execute_script!(file)
 			end
 		end
 	end
@@ -21,14 +21,29 @@ module Scripts
 		return file =~ /.*\.sql$/
 	end	
 
-	def self.execute_postgres_script!(script)
-		command = "psql -q -f #{SCRIPTS_DIR}#{script} -U #{USER_NAME} -d #{DB_NAME}"
+	def self.execute_script!(script)
 		puts "Executando: #{script}"
+		
+		sql = ""
+		File.open("#{SCRIPTS_DIR}#{script}", "r").each do |line|
+			sql << line
+		end
 
-		if system(command)
-			puts "\tScript executado com sucesso"
-		else
-			puts "\tFalha ao executar script"
+		Scripts.execute(sql)
+	end
+	
+	# Executa o sql passado
+	#
+	def self.execute(sql)
+		begin
+			con = ActiveRecord::Base.connection()
+			con.begin_db_transaction
+			con.execute(sql)
+			con.commit_db_transaction
+			
+			puts "-> Sql executado com sucesso!"
+		rescue StandardError => e
+			puts "-> Falha ao executar sql. (#{e})"
 		end
 	end
 
