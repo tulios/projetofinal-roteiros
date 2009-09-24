a14004266ae47c4d37873ea4a10ccfdcb5b2fc81
Ricardo <ricardo@ricardo-laptop.(none)>
2009-09-24 11:13:56 -0300

    consolidacao do caso de uso atribuir tag, atribuir dica e correcao do mapa de navegacao para atribuir tag

--- documentacao/DiagramasNavegacao/Atribuir tag/atribuirTag.mm
+++ documentacao/DiagramasNavegacao/Atribuir tag/atribuirTag.mm
@@ -1,7 +1,9 @@
-<map version="0.7.1">
-<node ID="_Freemind_Link_831026394" TEXT="Adicionar tag">
-<node ID="_" TEXT="Salvar" POSITION="right"/>
-<node TEXT="Retirar" POSITION="right"/>
-<node ID="_Freemind_Link_272647271" TEXT="Voltar" POSITION="right"/>
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1253799143309" ID="_Freemind_Link_831026394" MODIFIED="1253799143309" TEXT="Adicionar tag">
+<node CREATED="1253799143310" ID="_" MODIFIED="1253799143310" POSITION="right" TEXT="Salvar"/>
+<node CREATED="1253799143310" MODIFIED="1253799143310" POSITION="right" TEXT="Retirar"/>
+<node CREATED="1253799143310" ID="_Freemind_Link_272647271" MODIFIED="1253799169787" POSITION="right" TEXT="Pesquisar por tags"/>
+<node CREATED="1253799150341" ID="Freemind_Link_1644994428" MODIFIED="1253799154855" POSITION="right" TEXT="Voltar"/>
 </node>
 </map>
